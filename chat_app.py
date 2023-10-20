c1574789c8a9f33736efc0c5ca1c5c9e4580efaa
giuliana <julissag.v.ll@gmail.com>
2023-10-19 23:14:01 -0300

    gpt integration

--- chat_app.py
+++ chat_app.py
@@ -1,28 +1,31 @@
+import openai
 import streamlit as st
-import random
-import time
 
-st.title("Assistant Bot")
-#We use session state to store the chat history so we can display it in the chat message container.
-# Initialize chat history
+st.title("ChatGPT-like clone")
+
+openai.api_key = st.secrets["OPENAI_API_KEY"]
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
 if "messages" not in st.session_state:
     st.session_state.messages = []
-# Display chat messages from history on app rerun
+
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
-
-# React to user input
 if prompt := st.chat_input("What is up?"):
-    # Display user message in chat message container
+    st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
-    # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    response = f"Assistant: {prompt}"
-    
-    # Display assistant response in chat message container
     with st.chat_message("assistant"):
-        st.markdown(response)
-    # Add assistant response to chat history
-    st.session_state.messages.append({"role": "assistant", "content": response})
+        message_placeholder = st.empty()
+        full_response = ""
+        responses = openai.ChatCompletion.create( model=st.session_state["openai_model"],
+                                                 messages=[{"role": m["role"], "content": m["content"]}for m in st.session_state.messages],
+                                                 stream=True,)
+        for response in responses:
+            full_response += response.choices[0].delta.get("content", "")
+            message_placeholder.markdown(full_response + " ")
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
